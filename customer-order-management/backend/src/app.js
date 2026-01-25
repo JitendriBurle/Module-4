db69fbf2a65efe3ac0e014838a707ed40757aa39
JitendriBurle <ji28tu@gmail.com>
2026-01-25 20:10:51 +0530

    Added Customer-Order Management

--- customer-order-management/backend/src/app.js
+++ customer-order-management/backend/src/app.js
@@ -9,6 +9,6 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/customers', customerRoutes);
-app.use('api/orders', orderRoutes);
+app.use('/api/orders', orderRoutes);
 
 module.exports = app;
